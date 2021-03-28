3235f3a183f4df4765bcbb64377b8d115fa24de6
Harran Ali <harran.m@gmail.com>
2021-03-28 21:50:51 +0400

    update released version

--- gincoat/cmd/root.go
+++ gincoat/cmd/root.go
@@ -15,7 +15,7 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:     "gincoat",
-	Version: "v1.0-beta.7",
+	Version: "v1.0.1",
 	Short:   "Gincoat framework installer",
 	Long: `Gincoat installer helps you create new Gincoat framework projects.
 `,
