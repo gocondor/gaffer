8e7d4b4a059d6b4ec3752ed26c7b58d0e277749d
Harran Ali <harran.m@gmail.com>
2023-08-23 16:12:05 +0400

    implement generate eventjob

--- cmd/generate-eventjob.go
+++ cmd/generate-eventjob.go
@@ -5,19 +5,53 @@
 package cmd
 
 import (
+	"fmt"
+	"os"
+	"path/filepath"
+
 	"github.com/spf13/cobra"
 )
 
 var GenerateEventJobCmd = &cobra.Command{
-	Use:   "gen:eventjob",
+	Use:   "gen:eventjob [JobName]",
 	Short: "Create an event job",
 	Long: `Helps you generate a boilderplate code for event jobs
 example:
 gaffer gen:eventjob EventJobName
 
 `,
+	Args: cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		// TODO implement
+		pwd, err := os.Getwd()
+		if err != nil {
+			fmt.Println(err.Error())
+			os.Exit(1)
+		}
+		eventJobName := args[0]
+		if err != nil {
+			fmt.Println(err.Error())
+			os.Exit(1)
+		}
+
+		jfn := prepareJobFileName(eventJobName) + ".go"
+		ffnp := filepath.Join(pwd, "events/jobs", jfn)
+		jfs, err := os.Stat(ffnp)
+		if err != nil && !os.IsNotExist(err) {
+			fmt.Printf("problem creating the file: %v\n", ffnp)
+			os.Exit(1)
+		}
+
+		if jfs != nil {
+			fmt.Printf("file \"%v\" already exist\n", jfn)
+			os.Exit(1)
+		}
+		jobFile, err := os.Create(ffnp)
+		if err != err {
+			fmt.Println(err.Error())
+			os.Exit(1)
+		}
+		jobFile.WriteString(prepareJobContent(eventJobName))
+		jobFile.Close()
 	},
 }
 
